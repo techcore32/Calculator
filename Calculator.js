f5c0bdb669ff53836888e219f11c961ab15b5dcc
DiegoRT32 <sorpristerton43@proton.me>
2022-07-08 15:41:10 -0500

    Make all current buttons work

--- Calculator.js
+++ Calculator.js
@@ -12,14 +12,14 @@ buttons.addEventListener("click", e => {
     const displayValue = display.textContent;
     const {type} = key.dataset;
     const {previousKeyType} = calculator.dataset;
-   
+    
     //is key a number?
     if(type === "numbers"){
 
         if(displayValue === "0"){
             display.textContent = keyValue;
             ghostDisplay.textContent = keyValue;
-        }
+        } 
         else if(previousKeyType === "operator"){
             display.textContent = keyValue;
             ghostDisplay.textContent = keyValue;
@@ -42,26 +42,65 @@ buttons.addEventListener("click", e => {
         calculator.dataset.firstDigit = displayValue;
         calculator.dataset.operator = key.dataset.function;
     }
+    if(type === "operatorInstant"){
+        let operatorKeys = key.querySelectorAll("[data-type='operatorInstant']");
+        operatorKeys.forEach(el => {el.dataset.state = ""});
+        key.dataset.state = "selected";
 
-    if(type === "equal"){
-        let firstDigit = parseInt(calculator.dataset.firstDigit);
-        let secondDigit = parseInt(displayValue);
+        calculator.dataset.firstDigit = displayValue;
+        calculator.dataset.operator = key.dataset.function;
+        
+        let firstDigit = calculator.dataset.firstDigit;
         let operator = calculator.dataset.operator;
 
-        let result = "";
-    
-        
-        if(operator === "plus") result = firstDigit + secondDigit;
-        if(operator === "minus") result = firstDigit - secondDigit;
-        if(operator === "multiply")result = firstDigit * secondDigit;  
-        if(operator === "divide") result = firstDigit / secondDigit;
+        display.textContent = calculate(firstDigit,operator);
+    }
+    if(type === "equal"){
+        let firstDigit = calculator.dataset.firstDigit;
+        let secondDigit = displayValue;
+        let operator = calculator.dataset.operator;
 
-        display.textContent = result;
+        display.textContent = calculate(firstDigit,operator,secondDigit);
     }
 
     if(type === "clear"){
-        display.textContent = "0";
-        ghostDisplay.textContent = "";
+        
+        let erase = key.dataset.erase;
+
+        if(erase === "clear-entry"){
+            display.textContent = "0";
+        }   
+       else if(erase === "backSpace"){
+            display.textContent = display.textContent.slice(0, -1);
+            
+            if(display.textContent === "") display.textContent = "0";
+        }
+        else if(erase === "clear-global"){
+            display.textContent = "0";
+            ghostDisplay.textContent = "";
+        }
+        
+    }
+
+    if(type === "changer"){
+            display.textContent = -display.textContent;
     }
     calculator.dataset.previousKeyType = type;
+    
 })
+
+function calculate(firstDigit,operator,secondDigit){
+   firstDigit = parseFloat(firstDigit);
+   secondDigit = parseFloat(secondDigit);
+   let result = "";
+    
+    if(operator === "plus") result = firstDigit + secondDigit;
+    if(operator === "minus") result = firstDigit - secondDigit;
+    if(operator === "multiply")result = firstDigit * secondDigit;  
+    if(operator === "divide") result = firstDigit / secondDigit;
+    if(operator === "reciprocal") result = 1 / firstDigit;
+    if(operator === "square") result = firstDigit ** 2;
+    if(operator === "root-square") result = Math.sqrt(firstDigit);
+    if(operator === "percentage") result = (secondDigit/100) * firstDigit;
+        return result;
+}
